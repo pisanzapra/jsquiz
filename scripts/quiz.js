3e939bbae81fcb78218864cc38396fee1073177f
pisanzapra <pisanzapra@users.noreply.github.com>
2016-03-24 11:41:22 +0000

    Working for one question

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,35 @@
-alert("Hello from quiz.js");
+var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}];
+
+
+function Question (text, answer1, answer2, correctAnswerCode) {
+    this.text = text;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.correctAnswerCode = correctAnswerCode;
+}
+
+var question1 = new Question("What is the capital of the UK?", "London", "Brazil", 1);
+
+var question2 = new Question("Why is Wayne so annoying?", "He just is", "He isn't", 1);
+
+function findAnswer (correctAnswerCode) {
+    if (correctAnswerCode === 1) {
+        whichAnswer === answer1;
+    }
+    if (correctAnswerCode === 2) {
+        whichAnswer === answer2;
+    }
+}
+
+if whichAnswer = answer1;
+answer1.onClick(alert: "That's the correct answer!");
+answer2.
+
+if whichAnswer = answer2;
+answer2.onClick(alert: "That's the correct answer!");
+
+answeredCorrectly ()
+
+
+
+
